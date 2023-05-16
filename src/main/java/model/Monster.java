c7324a50e62d61b1deae3904a96d64e0f819f0bb
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2023-05-16 16:19:20 +0200

    Monster

---
+++ src/main/java/model/Monster.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.util.Objects;
+import java.sql.Blob;
+import java.util.Optional;
+
+public record Monster(String nameMonster, Optional<Blob> imageMonster) {
+    public Monster {
+        Objects.requireNonNull(nameMonster);
+    }
+}
