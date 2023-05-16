69399cb08f1d1161d36bfa0e2c944dcdc64378d4
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2023-05-16 16:10:16 +0200

    Added Class

--- src/main/java/model/Class.java
+++ src/main/java/model/Class.java
@@ -1,7 +1,10 @@
 package model;
 
+import java.util.Objects;
+
 public record Class(String nameClass, String descClass) {
     public Class {
-        
+        Objects.requireNonNull(nameClass);
+        Objects.requireNonNull(descClass);
     }
 }
