efff0dc9d14393299bda71b1a078174e26e5572c
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2023-05-19 02:24:29 +0200

    Added Relazione_DB2023.pdf

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 import db.Controller;
 import view.GUI;
 
-public class Hello {
+public class Main {
     
     public static void main(String[] args) {
         new GUI(new Controller()).display();
